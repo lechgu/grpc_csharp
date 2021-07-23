c3107f21c95c07e3168bec81aa4c1a266f860806
Lech Gudalewicz <lechgu@gmail.com>
2021-07-23 13:38:38 -0700

    Part One

---
+++ Client/Client.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Grpc.Core;
+using Shared;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string host = args[0];
+            int port = int.Parse(args[1]);
+            long x = long.Parse(args[2]);
+            string op = args[3];
+            long y = long.Parse(args[4]);
+
+            var channel = new Channel(
+                host,
+                port,
+                ChannelCredentials.Insecure
+                );
+            var client = new Svc.SvcClient(channel);
+            var reply = client.Calculate(new CalculateRequest
+            {
+                X = x,
+                Y = y,
+                Op = op
+            });
+            Console.WriteLine($"The calculated result is: {reply.Result}");
+        }
+    }
+}
---
+++ Service/Service.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Shared;
+
+public class MyService : Svc.SvcBase
+{
+    public override Task<CalculateReply> Calculate(CalculateRequest request, ServerCallContext context)
+    {
+        long result = -1;
+        switch (request.Op)
+        {
+            case "+":
+                result = request.X + request.Y;
+                break;
+            case "-":
+                result = request.X - request.Y;
+                break;
+            case "*":
+                result = request.X * request.Y;
+                break;
+            case "/":
+                if (request.Y != 0)
+                {
+                    result = (long)request.X / request.Y;
+                }
+                break;
+            default:
+                break;
+        }
+        return Task.FromResult(new CalculateReply { Result = result });
+    }
+
+    static void Main(string[] args)
+    {
+        int port = int.Parse(args[0]);
+        var server = new Server
+        {
+            Services = { Svc.BindService(new MyService()) },
+            Ports = { new ServerPort("0.0.0.0", port, ServerCredentials.Insecure) }
+        };
+        server.Start();
+        Console.WriteLine($"Server listening at port {port}. Press any key to terminate");
+        Console.ReadKey();
+    }
+}
