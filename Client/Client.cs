c1fed7576e73f2f832730f9853966853309be2fb
Lech Gudalewicz <lechgu@gmail.com>
2021-07-23 14:10:58 -0700

    Part Three

--- Client/Client.cs
+++ Client/Client.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Threading.Tasks;
 using Grpc.Core;
 using Shared;
 
@@ -7,13 +8,47 @@ namespace Client
 {
     class Program
     {
-        static void Main(string[] args)
+        static void DoCalculator(Svc.SvcClient client, String[] args)
         {
-            string host = args[0];
-            int port = int.Parse(args[1]);
             long x = long.Parse(args[2]);
             string op = args[3];
             long y = long.Parse(args[4]);
+            var reply = client.Calculate(new CalculateRequest
+            {
+                X = x,
+                Y = y,
+                Op = op
+            });
+            Console.WriteLine($"The calculated result is: {reply.Result}");
+        }
+        static async Task DoTimeSeries(Svc.SvcClient client, String[] args)
+        {
+            Console.WriteLine("doing time series");
+            using var duplex = client.Median();
+            var responseTask = Task.Run(async () =>
+            {
+                while (await duplex.ResponseStream.MoveNext())
+                {
+                    var resp = duplex.ResponseStream.Current;
+                    Console.WriteLine($"{resp.Timestamp}: {resp.Value}");
+                }
+            });
+            int ts = 1;
+            double temp = 10.0;
+            var rnd = new Random();
+            while (true)
+            {
+                await duplex.RequestStream.WriteAsync(new Temperature { Timestamp = ts, Value = temp });
+                ts += 1;
+                temp += rnd.NextDouble() - 0.5;
+            }
+        }
+
+        static async Task Main(string[] args)
+        {
+            string host = args[0];
+            int port = int.Parse(args[1]);
+
             var creds = new SslCredentials(
                 File.ReadAllText("cert/ca.pem")
             );
@@ -23,13 +58,14 @@ namespace Client
                 creds
                 );
             var client = new Svc.SvcClient(channel);
-            var reply = client.Calculate(new CalculateRequest
+            if (args.Length == 2)
             {
-                X = x,
-                Y = y,
-                Op = op
-            });
-            Console.WriteLine($"The calculated result is: {reply.Result}");
+                await DoTimeSeries(client, args);
+            }
+            else
+            {
+                DoCalculator(client, args);
+            }
         }
     }
 }
--- Service/Service.cs
+++ Service/Service.cs
@@ -1,52 +1,78 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 using Grpc.Core;
 using Shared;
 
-public class MyService : Svc.SvcBase
+namespace Service
 {
-    public override Task<CalculateReply> Calculate(CalculateRequest request, ServerCallContext context)
-    {
-        long result = -1;
-        switch (request.Op)
-        {
-            case "+":
-                result = request.X + request.Y;
-                break;
-            case "-":
-                result = request.X - request.Y;
-                break;
-            case "*":
-                result = request.X * request.Y;
-                break;
-            case "/":
-                if (request.Y != 0)
-                {
-                    result = (long)request.X / request.Y;
-                }
-                break;
-            default:
-                break;
-        }
-        return Task.FromResult(new CalculateReply { Result = result });
-    }
 
-    static void Main(string[] args)
+
+    public class MyService : Svc.SvcBase
     {
-        int port = int.Parse(args[0]);
-        var pair = new KeyCertificatePair(
-            File.ReadAllText("cert/service.pem"),
-            File.ReadAllText("cert/service-key.pem")
-            );
-        var creds = new SslServerCredentials(new[] { pair });
-        var server = new Server
+        public override Task<CalculateReply> Calculate(CalculateRequest request, ServerCallContext context)
         {
-            Services = { Svc.BindService(new MyService()) },
-            Ports = { new ServerPort("0.0.0.0", port, creds) }
-        };
-        server.Start();
-        Console.WriteLine($"Server listening at port {port}. Press any key to terminate");
-        Console.ReadKey();
+            long result = -1;
+            switch (request.Op)
+            {
+                case "+":
+                    result = request.X + request.Y;
+                    break;
+                case "-":
+                    result = request.X - request.Y;
+                    break;
+                case "*":
+                    result = request.X * request.Y;
+                    break;
+                case "/":
+                    if (request.Y != 0)
+                    {
+                        result = (long)request.X / request.Y;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return Task.FromResult(new CalculateReply { Result = result });
+        }
+
+        public override async Task Median(IAsyncStreamReader<Temperature> requestStream, IServerStreamWriter<Temperature> responseStream, ServerCallContext context)
+        {
+            Console.WriteLine("Median");
+            var vals = new List<double>();
+            while (await requestStream.MoveNext())
+            {
+                var temp = requestStream.Current;
+                vals.Add(temp.Value);
+                double med = 0;
+                if (vals.Count == 10)
+                {
+                    var arr = vals.ToArray();
+                    Array.Sort(arr);
+                    med = (arr[4] + arr[5]) / 2;
+                    vals.Clear();
+                    await responseStream.WriteAsync(new Temperature { Timestamp = temp.Timestamp, Value = med });
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int port = int.Parse(args[0]);
+            var pair = new KeyCertificatePair(
+                File.ReadAllText("cert/service.pem"),
+                File.ReadAllText("cert/service-key.pem")
+                );
+            var creds = new SslServerCredentials(new[] { pair });
+            var server = new Server
+            {
+                Services = { Svc.BindService(new MyService()) },
+                Ports = { new ServerPort("0.0.0.0", port, creds) }
+            };
+            server.Start();
+            Console.WriteLine($"Server listening at port {port}. Press any key to terminate");
+            Console.ReadKey();
+        }
     }
 }
